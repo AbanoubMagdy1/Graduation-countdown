cca47161f978a533bf1acad92d590239497d178e
Abanoub Magdy <bebomagdy666@gmail.com>
2023-10-02 15:26:31 +0300

    Add pwa capabilities

---
+++ addToHome.js
@@ -0,0 +1,28 @@
+const addBtn = document.querySelector("#addToHome")
+
+let addHomeEvent;
+
+function showInstallPrompt(){
+    addBtn.classList.add("show");
+}
+
+window.addEventListener('beforeinstallprompt', function(e) {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    addHomeEvent = e;
+    // Update UI notify the user they can add to home screen
+    showInstallPrompt();
+})
+
+addBtn.addEventListener("click", function(){
+    addHomeEvent.prompt();
+    addHomeEvent.userChoice.then(function(choiceResult) {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the Add to Home screen prompt');
+        } else {
+            console.log('User dismissed the Add to Home screen prompt');
+        }
+        addHomeEvent = null;
+    });
+}, {once: true});
---
+++ sw.js
@@ -0,0 +1,36 @@
+const cacheName = 'sw-cache-v1';
+
+const filesToCache = [
+    '/',
+    'index.html',
+    'style.css',
+    'script.js',
+    'graduationPhoto.jpg',
+]
+
+self.addEventListener('install', function(event) {
+    event.waitUntil( async function() {
+        const cache = await caches.open(cacheName);
+        await cache.addAll(filesToCache);
+    })
+})
+
+self.addEventListener('fetch', async function(event) {
+    event.respondWith( 
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    )
+})
+
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: data.icon,
+        image: data.image,
+    }
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    )
+})
